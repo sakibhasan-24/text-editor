e8ce8e482d5615a4d71aebb7bc53410a527bfb55
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-02 22:36:25 +0600

    align text

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const textArea = document.getElementById("text-area");
 const bold = document.getElementById("bold");
 const italic = document.getElementById("italic");
 const underline = document.getElementById("underline");
+const alignLeft = document.getElementById("align-left");
+const alignCenter = document.getElementById("align-center");
+const alignRight = document.getElementById("align-right");
 bold.addEventListener("click", function () {
-  textArea.classList.add("bold");
+  textArea.classList.toggle("bold");
 });
 
 underline.addEventListener("click", function () {
   textArea.classList.toggle("underline");
   underline.classList.toggle("active");
 });
+
+clickBtn(alignLeft, "align-left");
+clickBtn(alignRight, "align-right");
+clickBtn(alignCenter, "align-center");
+
+function clickBtn(targetBtn, className) {
+  targetBtn.addEventListener("click", function () {
+    textArea.classList.toggle(className);
+    targetBtn.classList.toggle("active");
+  });
+}
