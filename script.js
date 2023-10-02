0ad6237497db35a2ee1c4f1f4005a57c3106f17a
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-03 00:36:36 +0600

    set text input color

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ const underline = document.getElementById("underline");
 const alignLeft = document.getElementById("align-left");
 const alignCenter = document.getElementById("align-center");
 const alignRight = document.getElementById("align-right");
-bold.addEventListener("click", function () {
-  textArea.classList.toggle("bold");
+const colorSet = document.getElementById("color-set");
+colorSet.addEventListener("mouseleave", function (e) {
+  textArea.style.color = e.target.value;
 });
+textArea.style.color = colorSet;
+// bold.addEventListener("click", function () {
+//   textArea.classList.toggle("bold");
+// });
 
 underline.addEventListener("click", function () {
   textArea.classList.toggle("underline");
   underline.classList.toggle("active");
 });
-
+clickBtn(italic, "italic");
+clickBtn(bold, "bold");
 clickBtn(alignLeft, "align-left");
 clickBtn(alignRight, "align-right");
 clickBtn(alignCenter, "align-center");
